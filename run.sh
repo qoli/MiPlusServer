1d59dfa4b5a79964279e6db2f9780a551eff7991
Qoli Wong <llqoli@gmail.com>
2017-09-06 07:14:20 +0800

    改善運行腳本

--- run.sh
+++ run.sh
@@ -1,13 +1,3 @@
 #! /bin/sh
-
-case $1 in
-    start)
-      screen -S homebridge -dm /opt/bin/homebridge
-      screen -S miServer -dm /root/MiPlusServer/miServer.sh
-      ;;
-
-    stop)
-      killall screen
-      ;;
-esac
-exit 0
+screen -S homebridge -dm /opt/bin/homebridge
+screen -S miServer -dm /root/MiPlusServer/miServer.sh
